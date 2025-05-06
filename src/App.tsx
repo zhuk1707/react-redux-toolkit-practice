647421b06beb118c5f54156e4085b8a3ba6a8ec4
Evgeniy <charizardik64@gmail.com>
2025-05-06 16:42:13 +0300

    install redux-toolkit, react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reduxLogo from '/redux.svg'
 import './App.css'
 
 function App() {
@@ -9,25 +8,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="" target="_blank">
+          <img src={reduxLogo} className="logo redux" alt="" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <h1>Redux Toolkit</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
